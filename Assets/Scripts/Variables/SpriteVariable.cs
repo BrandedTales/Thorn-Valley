1bc80cbaa691ab9fc46263877374a28066131bee
BrandedTales <lordbrand@gmail.com>
2020-05-09 00:14:31 -0500

    0. Project Setup and prototyping modules installed.

---
+++ Assets/Scripts/Variables/SpriteVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BT.Brume
+{
+    [CreateAssetMenu(menuName = "Variable/Sprite")]
+    public class SpriteVariable : ScriptableObject
+    {
+        public Sprite value;
+    }
+}
